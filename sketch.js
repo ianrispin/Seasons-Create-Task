aa93ddb7eeb428d009e26e4bca1d03f02fabfe03
ianrispin <50210259+ianrispin@users.noreply.github.com>
2020-05-16 13:01:59 -0700

    Added a canvas and hills

--- sketch.js
+++ sketch.js
@@ -1,1 +1,11 @@
-//filler
+function setup() {
+    createCanvas(800, 800);
+    noStroke();
+}
+
+function draw() {
+    background('skyblue');
+    fill('green');
+    ellipse(200, 850, 700, 600);
+    ellipse(600, 800, 700, 600);
+}
